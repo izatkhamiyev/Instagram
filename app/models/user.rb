ed2f0cac7470ab879967cb9882320a081621eaa5
izatkhamiyev <izat0011@gmail.com>
2018-06-18 21:14:18 +0600

    Unique user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
  	validates :password, length: {minimum: 6, maximum: 30}
 	validates_email_format_of :email, message: 'The e-mail format is not correct!'
 	validates :username, :password, format: { with: /\A[0-9a-zA-Z_.\-]+\Z/, message: "Only alphanumeric characters, and -_."}
+
+	before_save { self.email = email.downcase }
+	before_save { self.username = username.downcase }
 end
