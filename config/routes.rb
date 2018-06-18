ee19ea963f64f3f5bfd6fc2cc0b7d17872699b9f
izatkhamiyev <izat0011@gmail.com>
2018-06-18 22:03:55 +0600

    sign up

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get '/contact', to: 'pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
+  resources :users
 end
